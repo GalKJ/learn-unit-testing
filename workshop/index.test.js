c20ac168e267ac419cb6fc3a352ddb4027a47894
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-10-31 15:24:03 +0000

    add test .index.test.js

---
+++ workshop/index.test.js
@@ -0,0 +1,5 @@
+test('Poke API', () => {
+    const result = makeUrl('pikachu');
+    const expected = "https://pokeapi.co/api/v2/pikachu";
+    equal(result, expected, 'makeUrl should return `https://pokeapi.co/api/v2/ + ${name}`');
+  });
