611630356f0e0bd71d51865f0cd1bc9ea75c4630
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-10-31 16:15:16 +0000

    add urlSearchParams logic

--- workshop/index.js
+++ workshop/index.js
@@ -1,5 +1,13 @@
 // "pikachu" -> "https://pokeapi.co/api/v2/pikachu"
 
-function makeUrl(name) {
-  return "https://pokeapi.co/api/v2/" + name;
+// function makeUrl(name) {
+//   return "https://pokeapi.co/api/v2/" + name;
+// }
+
+// *******************
+
+function searchParamsToObject (url) {
+  return new URLSearchParams(url);
 }
+
+searchParamsToObject();
--- workshop/index.test.js
+++ workshop/index.test.js
@@ -1,5 +1,12 @@
-test('Poke API', () => {
-    const result = makeUrl('pikachu');
-    const expected = "https://pokeapi.co/api/v2/pikachu";
-    equal(result, expected, 'makeUrl should return `https://pokeapi.co/api/v2/ + ${name}`');
-  });
+// test('Poke API', () => {
+//     const result = makeUrl('pikachu');
+//     const expected = "https://pokeapi.co/api/v2/pikachu";
+//     equal(result, expected, 'makeUrl should return `https://pokeapi.co/api/v2/ + ${name}`');
+//   });
+
+// *******************************
+
+// test(''
+
+
+// )
