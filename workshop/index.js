5601906f54e351e2ff85a64f0df196beed80f5e4
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-01 12:21:48 +0000

    add Object.fromEntries .index.js

--- workshop/index.js
+++ workshop/index.js
@@ -8,7 +8,13 @@
 
 function searchParamsToObject (url) {
  const objInstance =  new URLSearchParams(url);
-  console.log(objInstance);
+ const obj = Object.fromEntries(objInstance); 
+ console.log(Array.isArray(objInstance))//false
+ console.log(objInstance instanceof URLSearchParams)//true
+ console.log(objInstance instanceof Object)//true
+ console.log(obj);
+ console.log(Object.entries(obj));
+
 }
 
 searchParamsToObject("name=oliver&email=hello@oliverjam.es");
