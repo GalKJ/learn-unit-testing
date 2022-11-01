a3e2d4e1c80174d796b11edae17c9d70a141c4ad
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-01 16:07:32 +0000

    add is leap year function index.js

--- workshop/index.js
+++ workshop/index.js
@@ -23,7 +23,21 @@
 
 // *************************
 
-function isLeapYear (year) {
+// function to check leap year
 
-    
+function isLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
 }
+
+// take input
+const year = prompt('Enter a year:');
+
+
+
+isLeapYear(year);
