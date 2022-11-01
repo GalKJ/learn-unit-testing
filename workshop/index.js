c2560e78b4a226e4329f02270ec2c990079c3041
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-01 12:33:15 +0000

    add typeOf(obj) .index.js

--- workshop/index.js
+++ workshop/index.js
@@ -9,11 +9,12 @@
 function searchParamsToObject (url) {
  const objInstance =  new URLSearchParams(url);
  const obj = Object.fromEntries(objInstance); 
- console.log(Array.isArray(objInstance))//false
+ console.log(Array.isArray(obj))//false
  console.log(objInstance instanceof URLSearchParams)//true
  console.log(objInstance instanceof Object)//true
  console.log(obj);
  console.log(Object.entries(obj));
+ console.log(typeof(obj));
 
 }
 
